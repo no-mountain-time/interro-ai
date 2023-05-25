2b5a32c7f56fa3668917779c585a9c76102bfd4a
Felixljr <felixLJR@icloud.com>
2023-05-24 20:24:15 -0400

    Ratings and reviews added (#1)
    * Ratings and reviews added
    
    * Response componenet added
    
    * Added text area for response

--- pages/interview/questions.tsx
+++ pages/interview/questions.tsx
@@ -1,24 +1,22 @@
 import Progress from '@/components/Progress'
 import Input from '@/components/Input'
 import Button from '@/components/Button'
+import Response from '@/components/Response'
 import Modal from '@/components/Modal'
 
 export default function Questions() {
   return (
-
     <>
-    <h1>
-     Questions Start here:
-    </h1>
+      <h1>Questions Start here:</h1>
+      <Progress />
+      <div className='w-[60%] h-fit mx-auto rounded-lg bg-white mb-10'>
+        <p className='text-black'>Question</p>
+        <Input />
+        <Response />
+        <Button />
+      </div>
 
-<div className='w-[60%] h-[20%] mx-auto rounded-lg bg-white'>
-<p className='text-black'>Question</p>
-<Input />
-<Button />
-</div>
-<Progress />
-
-{/* Modal when complete */}
-</>
+      {/* Modal when complete */}
+    </>
   )
 }
