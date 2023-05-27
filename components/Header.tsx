58d736100272399ac151160635a154ce4c82ab22
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-27 13:48:59 -0400

    nav items added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 const navigation = [
   { name: 'Start Interview', href: '#', current: true },
@@ -46,47 +47,57 @@ export default function Header() {
                 </div>
                 <div className='hidden sm:ml-6 sm:block'>
                   <div className='flex space-x-4'>
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                    <div className='flex'>
+                      <div className='flex flex-shrink-0 items-center'></div>
+                      <div className='hidden md:ml-6 md:flex md:space-x-8'>
+                        <Link
+                          href='/interview'
+                          className='inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                        >
+                          Start Interview
+                        </Link>
+                        <Link
+                          href='/stats'
+                          className='inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                        >
+                          Performance
+                        </Link>
+                        <Link
+                          href='/'
+                          className='inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                        >
+                          SignIn/Register
+                        </Link>
+
+                        <Link
+                          href='/'
+                          className='inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                        >
+                          Sign Out
+                        </Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-              </div>
+              <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'></div>
             </div>
           </div>
 
-          <Disclosure.Panel className='sm:hidden'>
-            <div className='space-y-1 px-2 pb-3 pt-2'>
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as='a'
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+          <Disclosure.Panel className='md:hidden'>
+            <div className='space-y-1 pb-3 pt-2'>
+              <Disclosure.Button className='block border-l-4 hover:border-brand hover:bg-gray-100 py-2 pl-3 pr-4 text-base text-gray-600 font-medium w-[100%]'>
+                <Link href='/interview'>Start Interview</Link>
+              </Disclosure.Button>
+              <Disclosure.Button className='block border-l-4 hover:border-brand hover:bg-gray-100 py-2 pl-3 pr-4 text-base text-gray-600 font-medium w-[100%]'>
+                <Link href='/stats'>Performance</Link>
+              </Disclosure.Button>
+              <Disclosure.Button className='block border-l-4 hover:border-brand hover:bg-gray-100 py-2 pl-3 pr-4 text-base text-gray-600 font-medium w-[100%]'>
+                <Link href='/'>SignIn/Register</Link>
+              </Disclosure.Button>
+              <Disclosure.Button className='block border-l-4 hover:border-brand hover:bg-gray-100 py-2 pl-3 pr-4 text-base text-gray-600 font-medium w-[100%]'>
+                <Link href='/'>Sign Out</Link>
+              </Disclosure.Button>
             </div>
           </Disclosure.Panel>
         </>
