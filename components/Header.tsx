877fa1715a58fb2cae7ac1f11ab19eb36b440c73
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-27 14:30:22 -0400

    state mgmt boiler plate

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,6 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-
-const navigation = [
-  { name: 'Start Interview', href: '#', current: true },
-  { name: 'Performance', href: '#', current: false },
-  { name: 'SignIn/Register', href: '#', current: false },
-  { name: 'Sign out', href: '#', current: false },
-]
 
 function classNames(...classes:any) {
   return classes.filter(Boolean).join(' ')
