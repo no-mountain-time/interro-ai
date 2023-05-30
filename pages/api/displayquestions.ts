4be5faabb73ac28ae77cff88d06c7554840ae701
soxg <jdgray0224@gmail.com>
2023-05-30 15:43:15 -0400

    inner join sql query for display table

--- pages/api/displayquestions.ts
+++ pages/api/displayquestions.ts
@@ -8,6 +8,7 @@ export default async function handler(
   const client = await db.connect();
  
   try {
+    
     await client.sql`CREATE TABLE );`;
     const names = ['Fiona', 'Lucy'];
     await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
@@ -15,6 +16,18 @@ export default async function handler(
     return response.status(500).json({ error });
   }
  
-  const pets = await client.sql`SELECT * FROM Pets;`;
-  return response.status(200).json({ pets });
+  const questions = await client.sql`SELECT 
+    questions.id,
+    questions.text,
+    questions.difficulty_level,
+    questions.time_allotted,
+    topics.name AS topic_name
+FROM 
+    questions
+INNER JOIN 
+    question_topics ON questions.id = question_topics.question_id
+INNER JOIN 
+    topics ON question_topics.topic_id = topics.id;
+`;
+  return response.status(200).json({ questions });
 }
