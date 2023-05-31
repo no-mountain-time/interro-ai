1b2497aad83464c8b184f0704fdade21b123ab96
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-31 09:52:36 -0400

    new input component

--- pages/interview/index.tsx
+++ pages/interview/index.tsx
@@ -1,8 +1,17 @@
+import InputSubject from '@/components/InputSubject'
+
 export default function Interview() {
   return (
-    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 outline-dotted outline-red-700'>
-      <div className='mx-auto max-w-3xl bg-white outline-dotted outline-lime-700'>
-        <p>Build an interview</p>
+    <div className='mx-auto w-[70%] max-w-7xl px-4 rounded-r-lg sm:px-6 lg:px-8 outline-dotted outline-red-700'>
+      <div className='mx-auto max-w-3xl bg-white outline-dotted rounded-lg'>
+        <p className='text-slate-800'>Build an interview</p>
+        <div className='flex flex-row'>
+          <div>
+            <p className='text-slate-800'>Enter subjects:</p>
+
+          </div>
+          <div></div>
+        </div>
       </div>
     </div>
   )
