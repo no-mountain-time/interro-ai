4486dc55df80bb41bfdf157529a29476a414c4b3
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-31 13:00:00 -0400

    Interview build consolidated and complete w/o auto

--- pages/interview/index.tsx
+++ pages/interview/index.tsx
@@ -1,16 +1,23 @@
 import Button from '@/components/Button'
 import { BsPlayFill } from 'react-icons/bs'
-import InputSubject from '@/components/InputSubject'
 import { useRef } from 'react'
 
-
 export default function Interview() {
+  const topic1Ref = useRef<HTMLInputElement>(null)
+  const topic2Ref = useRef<HTMLInputElement>(null)
+  const topic3Ref = useRef<HTMLInputElement>(null)
+
   const companyRef = useRef<HTMLInputElement>(null)
 
   const handleInterviewStart = (e: any) => {
     e.preventDefault()
     console.log('Interview started')
+    //Controll logic needed to ensure only one side is selected or have 2 different buttons
+    //This can be saved for last since it's not a priority for demo purposes
     console.log(companyRef.current?.value)
+    console.log(topic1Ref.current?.value)
+    console.log(topic2Ref.current?.value)
+    console.log(topic3Ref.current?.value)
   }
 
   return (
@@ -22,7 +29,34 @@ export default function Interview() {
       <div className='flex flex-row justify-evenly'>
         <div className='w-1/3'>
           <p className='text-slate-800 text-center'>Select topics</p>
-          <InputSubject />
+          <div>
+            <div className='mt-2'>
+              <input
+                type='text'
+                name='topic1'
+                id='topic1'
+                ref={topic1Ref}
+                className='block w-full mb-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                placeholder='React, Algorithms, etc.'
+              />
+              <input
+                type='text'
+                name='topic2'
+                id='topic2'
+                ref={topic2Ref}
+                className='block w-full mb-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                placeholder='Node, CSS, etc.'
+              />
+              <input
+                type='text'
+                name='topic3'
+                id='topic3'
+                ref={topic3Ref}
+                className='block w-full mb-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                placeholder='System Design, JavaScript, etc.'
+              />
+            </div>
+          </div>
         </div>
 
         {/* divider between topic and companies */}
