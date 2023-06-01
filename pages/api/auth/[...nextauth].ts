f10d3942a54d57142c186b6cb40f8530937dfa9f
jaysenjonsin <100107689+jaysenjonsin@users.noreply.github.com>
2023-05-31 19:14:44 -0700

    O auth (#8)
    * add google provider
    
    * add login page
    
    * conditional header rendering

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,19 +5,15 @@ import GoogleProvider from 'next-auth/providers/google';
 export const authOptions = {
   providers: [
     GithubProvider({
-        //@ts-ignore
-      clientId: process.env.GITHUB_ID,
-      //@ts-ignore
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
     GoogleProvider({
-        //@ts-ignore
-      clientId: process.env.GOOGLE_CLIENT_ID,
-        //@ts-ignore
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ]
-}
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+};
 
 export default NextAuth(authOptions);
-
