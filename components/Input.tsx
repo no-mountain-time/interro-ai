8d6a92b3bc2e9ba1bfca8224c4698af85e57426b
Felixljr <felixLJR@icloud.com>
2023-06-03 13:18:13 -0400

    finishing up prompt for interview questions (#13)
    * team
    
    * edit
    
    * interview
    
    * sub btn
    
    * finishing up prompt for interview questions
    
    * merge dev and question
    
    * Felix/stats (#10)
    
    * rating component clean up
    
    * ratings and stats combined
    
    * modal appears
    
    * modal set to show over stats
    
    * modal functioning
    
    * q and a populating in feedback modal
    
    * modal set for showing feedback
    
    * changed icon
    
    ---------
    
    Co-authored-by: Andrew Luu <drewluu@Andrews-MacBook-Air.local>
    Co-authored-by: Andrew Luu <drewluu24@gmail.com>
    Co-authored-by: MasonShelton <dentworx68@gmail.com>

--- components/Input.tsx
+++ components/Input.tsx
@@ -3,19 +3,16 @@ export default function Input() {
   return (
     <div>
       <label
-        htmlFor='email'
+        htmlFor='question'
         className='block text-sm font-medium leading-6 text-gray-900'
       >
-        Email
+        Question
       </label>
       <div className='mt-2'>
-        <input
-          type='email'
-          name='email'
-          id='email'
-          className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-          placeholder='you@example.com'
-        />
+        <p className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  sm:text-sm sm:leading-6'
+        >
+          Chat GPT Question
+        </p>
       </div>
     </div>
   )
--- pages/api/gpt/chatGPTtests.ts
+++ pages/api/gpt/chatGPTtests.ts
@@ -19,49 +19,55 @@ const arrayOfAnswers = [
 
 ]
 
+const sets = [{gpt: "", user: ""}, {gpt: "", user: ""}]
+
 const openai = new OpenAIApi(configuration)
-const int_question = 'What excites or interests you about coding?'
+const int_topic = 'What excites or interests you about coding?'
 const int_answer =
   'Being able to create anything I want, I also love working with computers'
-let transcript =
-  "Recruiter: Hello, thanks for joining me today for the interview. Can you tell me about your experience with Node.js development? Interviewee: I have been building node application for like 10 years. Recruiter: That's impressive. Can you share with me some of your notable projects that you’ve built using Node.js? Inteviewee: I build the backend of facebook Recruiter: Wow, building the backend of Facebook is definitely impressive. What kind of challenges did you face while building it, and how did you overcome them? Interviewee: The hardest thing was trying to scale to 2 billion users, I don't want to get into it though"
+// let transcript =
+//   "Recruiter: Hello, thanks for joining me today for the interview. Can you tell me about your experience with Node.js development? Interviewee: I have been building node application for like 10 years. Recruiter: That's impressive. Can you share with me some of your notable projects that you’ve built using Node.js? Inteviewee: I build the backend of facebook Recruiter: Wow, building the backend of Facebook is definitely impressive. What kind of challenges did you face while building it, and how did you overcome them? Interviewee: The hardest thing was trying to scale to 2 billion users, I don't want to get into it though"
 // let indexOfQandA = 0
+let transcript = ''
 
 const runPrompt = async () => {
   //if there is no transcript then the interview just started. So send the first question and ans as separate variables
-  // if (transcript === '') {
-  //   const completion = await openai.createChatCompletion({
-  //     model: 'gpt-3.5-turbo',
-  //     messages: [
-  //       {
-  //         role: 'system',
-  //         content: `You are a recruiter working at a tech company conducting and interview and you ask the following question: ${int_question}. Respond to the answer in a way that would be appropriate for a recruiter to continue the conversation.`,
-  //       },
-  //       { role: 'user', content: `${int_answer}` },
-  //     ],
-  //   })
-
-  //   transcript += `Interview Question: ${int_question}, Interviewees Response: ${int_answer}, Your Response to the Interviewee: ${completion.data.choices[0].message.content}`
-
-  // } else {
-
+  if (transcript === '') {
     const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [
         {
           role: 'system',
-          content: `You are a recruiter with technical knowledge working at a tech company conducting an interview (you either just started it or you are in the middle of it), this is the transcript of the interview so far ${transcript}; if the transcript is empty or the user just says 'hello'. Ask an appropriate question to start, or follow up question, or respond to the answer in a way that would be appropriate for a recruiter to continue the conversation, feel free to ask reallyhard questions within the scope. This is an interview for a node developer`,
+          content: `You are a technical recruiter working at a company that is hiring a software engineer. You are conducting an interview and choose one of the following questions to ask on react. Respond to the answer while giving feedback and an overall grade out of 100. Make sure to grade the answer harshly.`,
         },
-        // { role: 'user', content: `hello` },
+        { role: 'user', content: `${int_answer}` },
       ],
     })
+  }
+  
 
-   // transcript += `Interview Question: ${arrayOfQuestions[indexOfQandA]}, Interviewees Response: ${arrayOfAnswers[indexOfQandA]}, Your Response to the Interviewee: ${completion.data.choices[0].message.content}`
-  // }
+    //   transcript += `Interview Question: ${int_question}, Interviewees Response: ${int_answer}, Your Response to the Interviewee: ${completion.data.choices[0].message.content}`
 
-  // console.log(completion.data.choices[0].message);
-  // console.log(completion.data)
- console.log(completion.data.choices[0].message.content)
+    // } else {
+
+    // const completion = await openai.createChatCompletion({
+    //   model: 'gpt-3.5-turbo',
+    //   messages: [
+    //     {
+    //       role: 'system',
+    //       content: `You are a recruiter with technical knowledge working at a tech company conducting an interview (you either just started it or you are in the middle of it), this is the transcript of the interview so far ${transcript}; if the transcript is empty or the user just says 'hello'. Ask an appropriate question to start, or follow up question, or respond to the answer in a way that would be appropriate for a recruiter to continue the conversation, feel free to ask reallyhard questions within the scope. This is an interview for a node developer`,
+    //     },
+    //     // { role: 'user', content: `hello` },
+    //   ],
+    // })
+
+    // transcript += `Interview Question: ${arrayOfQuestions[indexOfQandA]}, Interviewees Response: ${arrayOfAnswers[indexOfQandA]}, Your Response to the Interviewee: ${completion.data.choices[0].message.content}`
+    // }
+
+    // console.log(completion.data.choices[0].message);
+    // console.log(completion.data)
+    console.log(completion.data.choices[0].message.content)
+  
  //console.log(transcript);
 }
 
